cbcb3adde116d809d96199c358375a5dacb9642a
lee-hokyoung <supereggsong@gmail.com>
2020-03-01 10:17:14 +0900

    facet 만들기 시작부분

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var facetRouter = require("./routes/facet");
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/facet", facetRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
